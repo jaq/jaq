0b7f7022aede9408303fd647cf67510e97960684
Carlos Javier Prados Hijon <carlos.prados@gmail.com>
2011-10-06 13:06:47 +0200

    Some refactors, new classes and logger introduction

---
+++ src/main/java/org/jaq/DataBasePing.java
@@ -0,0 +1,11 @@
+package org.jaq;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: charlie
+ * Date: 10/6/11
+ * Time: 12:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DataBasePing {
+}
---
+++ src/main/java/org/jaq/QueryResultPrinter.java
@@ -0,0 +1,11 @@
+package org.jaq;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: charlie
+ * Date: 10/6/11
+ * Time: 1:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QueryResultPrinter {
+}
