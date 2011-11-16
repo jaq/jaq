6eb0821e217ff0cf8542809721d15bfe942dd5bb
Carlos Javier Prados Hijon <carlos.prados@gmail.com>
2011-11-16 13:22:59 +0100

    improved printHelp

--- src/main/java/org/jaq/Jaq.java
+++ src/main/java/org/jaq/Jaq.java
@@ -13,6 +13,10 @@ import java.io.PrintWriter;
 public class Jaq {
 
     private static Logger sm_logger = Logger.getLogger(Jaq.class);
+    public static final int SPACES_BEFORE_OPTION = 5;
+    public static final int SPACES_BEFORE_OPTION_DESCRIPTION = 3;
+    public static final int MAX_PRINT_COLUMNS = 80;
+    public static final boolean DISPLAY_USAGE = true;
 
     public Options getOptions() {
         return m_options;
@@ -32,7 +36,8 @@ public class Jaq {
 
     }
 
-    private void printHelp(Options options, int printedRowWidth, String header, String footer, int spacesBeforeOption, int spacesBeforeOptionDescription, boolean displayUsage) {
+    private void printHelp(String _header, String _footer) {
+
         String commandLineSyntax = "java -cp ApacheCommonsCLI.jar";
 
         PrintWriter writer = new PrintWriter(System.out);
@@ -40,14 +45,14 @@ public class Jaq {
         HelpFormatter helpFormatter = new HelpFormatter();
         helpFormatter.printHelp(
                 writer,
-                printedRowWidth,
+                MAX_PRINT_COLUMNS,
                 commandLineSyntax,
-                header,
-                options,
-                spacesBeforeOption,
-                spacesBeforeOptionDescription,
-                footer,
-                displayUsage);
+                _header,
+                m_options,
+                SPACES_BEFORE_OPTION,
+                SPACES_BEFORE_OPTION_DESCRIPTION,
+                _footer,
+                DISPLAY_USAGE);
 
         writer.flush();
     }
@@ -68,15 +73,14 @@ public class Jaq {
         System.out.println();
 
 
-        Jaq jaq= new Jaq();
-        Options options= jaq.getOptions();
+        Jaq jaq = new Jaq();
+        Options options = jaq.getOptions();
 
-        int maxPrintColumns = 80;
         if (_args.length < 1) {
 
             HelpFormatter usageFormatter = new HelpFormatter();
-            usageFormatter.printUsage(new PrintWriter(System.out), maxPrintColumns, applicationName, options);
-            jaq.printHelp(options, maxPrintColumns, "Jaq help", "Enjoy using Jaq", 5, 3, true);
+            usageFormatter.printUsage(new PrintWriter(System.out), MAX_PRINT_COLUMNS, applicationName, options);
+            jaq.printHelp("Jaq help", "Enjoy using Jaq");
         }
 
         jaq.displayProvidedCommandLineArguments(_args);
@@ -92,7 +96,7 @@ public class Jaq {
                 sm_logger.debug("You want to execute sql on Data Base!");
             }
             if (commandLine.hasOption('h') || commandLine.hasOption("help")) {
-                jaq.printHelp(options, maxPrintColumns, "GNU HELP", "End of GNU Help", 5, 3, true);
+                jaq.printHelp("GNU HELP", "End of GNU Help");
             }
         } catch (ParseException parseException) {
             sm_logger.error("Encountered exception while parsing command line:", parseException);
